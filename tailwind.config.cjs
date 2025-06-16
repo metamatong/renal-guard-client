fcb72bf4392ca2e27b831278208ccd3ef56b7d41
Kyle Cheon <kcheon@my.bcit.ca>
2025-06-15 22:48:12 -0700

    refactor: add custom font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        /* Make Pretendard the default sans stack */
+        sans: ['Pretendard', ...defaultTheme.fontFamily.sans],
+
+        /* Optional shortcut utility: `font-pretendard` */
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
