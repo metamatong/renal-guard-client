7b0b2ee4de95fd83134840e5d1e4408579df0972
Kyle Cheon <kcheon@my.bcit.ca>
2025-06-15 21:38:56 -0700

    fix: install image tools

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,4 @@
-declare module '*.svg?component' {
-  import * as React from 'react';
-  const C: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default C;
+declare module '*?*' {
+  const src: string;
+  export default src;
 }
-
-declare module '*.svg';
-declare module '*.png';
-declare module '*.css';
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr';
+import { imagetools } from 'vite-imagetools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(),
+    imagetools(),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
