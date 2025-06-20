0b645b40b28227e09f7e9f55ef9f80901edf32ad
Kyle Cheon <kcheon@my.bcit.ca>
2025-06-19 23:41:42 -0700

    refactor: remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import {BrowserRouter} from 'react-router-dom';
-import AppRoutes from '@/routes/AppRouter.tsx';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import LandingPage from '@/pages/Landing';
+import SignIn from '@/pages/SignIn';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/scan" element={<Scan />} />
+        <Route path="/processing" element={<Processing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/meal/:mealId" element={<Meal />} />
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
