b0e857429f97534bddc7a63820f9dee75edda294
Kyle Cheon <kcheon@my.bcit.ca>
2025-06-19 23:13:28 -0700

    refactor: setup redux toolkit

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,9 +1,5 @@
-import { useDispatch, useSelector } from 'react-redux';
-import type { TypedUseSelectorHook } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './index';
 
-/**
- * Typed versions of useDispatch and useSelector hooks for the app.
- */
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
