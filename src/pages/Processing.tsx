76842c1cea7ed4d6dd9266e4c20c85695de643de
Kyle Cheon <kcheon@my.bcit.ca>
2025-06-21 12:36:21 -0700

    feat: complete processing page ui

--- src/pages/Processing.tsx
+++ src/pages/Processing.tsx
@@ -1,51 +1,61 @@
-import React, {useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {ShieldCheck} from 'lucide-react';
-import LoadingImg from '@/assets/loading-image.png?as=src';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import clsx from 'clsx';
-
+import CancelImg from '@/assets/cancel-button.png?as=src';
+import LoadingImg from '@/assets/loading-image.png?as=src';
+import PageWrapper from '@/components/layouts/PageWrapper';
 
 const Processing: React.FC = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     // TODO: Replace this with actual API call to your backend
-    // This simulates waiting for the analysis to complete.
     const timer = setTimeout(() => {
       // Navigate to the Meal results page after processing
-      navigate('/meal');
-    }, 40000); // 4-second delay for demonstration
+      navigate('/dashboard');
+    }, 4000); // 4-second delay for demonstration
 
     // Cleanup the timer if the component unmounts
     return () => clearTimeout(timer);
   }, [navigate]);
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4 text-center'>
-      {/* Animated Icon Placeholder */}
-      <div className='relative mb-8'>
-        <img
-          src={LoadingImg}
-          alt='Loading'
-          className={clsx(
-            'w-[15em] h-auto',
-            'text-blue-400',
-            'animate-[spin_8s_linear_infinite,pulse_2s_ease-in-out_infinite]'
-          )}
-        />
-      </div>
-
-      <h1 className='text-2xl font-bold mb-2'>Getting your meal's</h1>
-      <h2 className='text-2xl font-bold mb-8'>nutritional breakdown...</h2>
-
-      {/* Optional cancel button */}
-      <button
-        onClick={() => navigate(-1)} // Go back to the previous page (Scan)
-        className='mt-8 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full'
+    <PageWrapper extraComponents={{ hasFooter: false, hasBottomNavigation: false }}>
+      <div
+        className={clsx(
+          'flex flex-col flex-1 w-full min-h-[calc(100vh-256px)] items-center justify-center bg-gray-100 px-4'
+        )}
       >
-        Cancel
-      </button>
-    </div>
+        <div className="flex flex-col items-center text-center text-white">
+          <span className="mb-2 text-[1.5em] text-gray-900 font-semibold font-bold">Getting your meal&apos;s</span>
+          <span className="mb-8 text-[1.5em] text-gray-900 font-semibold font-bold">nutritional breakdown...</span>
+
+          <div className="relative mb-8">
+            <img
+              src={LoadingImg}
+              alt="Loading"
+              className={clsx(
+                'h-auto w-[20em]',
+                'text-blue-400',
+                'animate-[spin_8s_linear_infinite,pulse_2s_ease-in-out_infinite]'
+              )}
+            />
+          </div>
+
+          <button
+            onClick={() => navigate(-1)}
+          >
+            <img
+              src={CancelImg}
+              alt="Cancel"
+              className={clsx(
+                'h-auto w-[3.75em]',
+              )}
+            />
+          </button>
+        </div>
+      </div>
+    </PageWrapper>
   );
 };
 
